a0d9e9ede7f9cbed883922ef77d356d51838c19b
yeonh0 <nanyeonho@gmail.com>
2024-06-10 05:05:23 +0100

    1

--- .modules.order.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/home/dev/OS_DD/modules.order := {   echo /home/dev/OS_DD/mymodule.o; :; } > /home/dev/OS_DD/modules.order
--- .mymodule.mod.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/home/dev/OS_DD/mymodule.mod := printf '%s\n'   mymodule.o | awk '!x[$$0]++ { print("/home/dev/OS_DD/"$$0) }' > /home/dev/OS_DD/mymodule.mod
